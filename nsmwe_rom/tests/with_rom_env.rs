e499187fd95840dc30d0d10a55706ef9ca60dccd
Jakub Szewczyk <git@kubasz.xyz>
2021-05-02 15:46:48 +0100

    Testing on a ROM file

---
+++ nsmwe_rom/tests/with_rom_env.rs
@@ -0,0 +1,11 @@
+use std::env;
+
+use nsmwe_rom::Rom;
+
+#[test]
+#[ignore]
+fn test_with_rom_env() {
+    let rom_path = env::var_os("ROM_PATH").expect("ROM_PATH not set");
+    assert!(std::fs::metadata(&rom_path).expect("ROM_PATH invalid").is_file());
+    Rom::from_file(rom_path).expect("Rom parse error encountered");
+}
