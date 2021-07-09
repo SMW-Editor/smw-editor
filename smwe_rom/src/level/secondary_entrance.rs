80e6949e73542aa273330a9afac35a71685d02e0
Adanos020 <adanos020@gmail.com>
2021-07-09 17:54:32 +0100

    Add missing bitfield comments

--- smwe_rom/src/level/secondary_entrance.rs
+++ smwe_rom/src/level/secondary_entrance.rs
@@ -31,28 +31,26 @@ impl SecondaryEntrance {
     pub fn destination_level(&self) -> u16 {
         // dddddddd -------- -------- ----D---
         // destination_level = Ddddddddd
-
-        ((self.0[3] as u16 & 0b111) << 5) | self.0[0] as u16
+        let hi = (self.0[3] as u16 & 0b1000) << 5;
+        let lo = self.0[0] as u16;
+        hi | lo
     }
 
     pub fn bg_initial_pos(&self) -> u8 {
         // -------- bb------ -------- --------
         // by_initial_pos = bb
-
         self.0[1] >> 6
     }
 
     pub fn fg_initial_pos(&self) -> u8 {
         // -------- --ff---- -------- --------
         // fg_initial_pos = ff
-
         self.0[1] >> 4
     }
 
     pub fn entrance_xy_pos(&self) -> (u8, u8) {
         // -------- ----yyyy xxx----- --------
         // entrance_xy_pos = (xxx, yyyy)
-
         let x = self.0[2] >> 5;
         let y = self.0[1] & 0b1111;
         (x, y)
@@ -61,7 +59,6 @@ impl SecondaryEntrance {
     pub fn screen_number(&self) -> u8 {
         // -------- -------- ---SSSSS --------
         // screen_number = SSSSS
-
         self.0[2] & 0b11111
     }
 }
