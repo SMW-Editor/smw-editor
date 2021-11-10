c87b2c6ffc60350cb054a9007952110c4b2adb01
Adanos020 <adanos020@gmail.com>
2021-11-10 00:09:37 +0000

    Implement most stack operations

--- smwe_rom/src/disassembler/registers.rs
+++ smwe_rom/src/disassembler/registers.rs
@@ -1,6 +1,35 @@
 // This module will probably not define every register of the 65816,
 // only those that are needed for disassembly.
 
+/// Accumulator
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
+pub struct ARegister(pub u16);
+
+/// X Index
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
+pub struct XRegister(pub u16);
+
+/// Y Index
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
+pub struct YRegister(pub u16);
+
+/// Data Bank
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
+pub struct DBRegister(pub u8);
+
+/// Direct Page
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
+pub struct DPRegister(pub u16);
+
+/// Program Bank
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
+pub struct PBRegister(pub u8);
+
+/// Program Counter
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
+pub struct PCRegister(pub u8);
+
+/// Processor Status
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
 pub struct PRegister(pub u8);
 
