ce28b24feed15e24ba1e93aab6d8c91a92977ade
Adanos020 <adanos020@gmail.com>
2023-04-24 20:36:48 +0100

    Fix cpu benchmark

--- wdc65816/benches/cpu.rs
+++ wdc65816/benches/cpu.rs
@@ -3,19 +3,21 @@
 #![feature(test)]
 
 extern crate test;
+extern crate wdc65816;
 
 use test::Bencher;
+use wdc65816::Cpu;
 
 struct DummyMem(&'static [u8]);
 
 impl wdc65816::Mem for DummyMem {
-    fn load(&mut self, bank: u8, addr: u16) -> u8 {
+    fn load(&mut self, addr: u32) -> u8 {
         // Get from array or return 0
         // This places all vectors at $0000
-        *self.0.get((bank as usize) << 16 | addr as usize).unwrap_or(&0)
+        *self.0.get(addr as usize).unwrap_or(&0)
     }
 
-    fn store(&mut self, _bank: u8, _addr: u16, _value: u8) {}
+    fn store(&mut self, _addr: u32, _value: u8) {}
 }
 
 /// This is a bad benchmark for the WDC65816. It only ever runs in emulation mode with 8-bit acc and
@@ -27,21 +29,19 @@ impl wdc65816::Mem for DummyMem {
 #[bench]
 fn cpu_simple(b: &mut Bencher) {
     static CODE: &'static [u8] = &[
-        0xA9, 0x00,         // lda #0
-        0xA2, 0x00,         // ldx #0
-        0xA0, 0x00,         // ldy #0
-        0x9A,               // txs
-
+        0xA9, 0x00, // lda #0
+        0xA2, 0x00, // ldx #0
+        0xA0, 0x00, // ldy #0
+        0x9A, // txs
         // Let the PPU do some work
         // Disable forced blank and set brightness to max
-        0xA9, 0x0F,         // lda #$0F
-        0x8D, 0x00, 0x21,   // sta $2100
+        0xA9, 0x0F, // lda #$0F
+        0x8D, 0x00, 0x21, // sta $2100
         // Enable all layers on the main screen
-        0xA9, 0x1F,         // lda #$1F
-        0x8D, 0x2C, 0x21,   // sta $212C
-
+        0xA9, 0x1F, // lda #$1F
+        0x8D, 0x2C, 0x21, // sta $212C
         // Repeat, just for fun
-        0x4C, 0x00, 0x00,   // jmp $0000
+        0x4C, 0x00, 0x00, // jmp $0000
     ];
 
     let mut cpu = Cpu::new(DummyMem(CODE));
@@ -53,7 +53,9 @@ fn cpu_simple(b: &mut Bencher) {
         loop {
             cy += cpu.dispatch();
 
-            if cpu.pc == 0 { break; }
+            if cpu.pc == 0 {
+                break;
+            }
         }
 
         cy
