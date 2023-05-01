4f34852bc6b72649af24ee49d3466ae45efa5d30
Adanos020 <adanos020@gmail.com>
2023-05-01 12:54:02 +0100

    Move all library crates into crates/

--- crates/smwe-project/src/lib.rs
+++ crates/smwe-project/src/lib.rs
@@ -17,7 +17,7 @@ impl Project {
         let old_rom_data = SmwRom::from_file(&rom_path)?;
         // TODO: check smc/sfc
         let mut rom = Rom::new(std::fs::read(&rom_path)?[0x200..].to_vec());
-        rom.load_symbols(include_str!("../../symbols/SMW_U.sym"));
+        rom.load_symbols(include_str!("../../../symbols/SMW_U.sym"));
 
         Ok(Self { title: String::from("Test Project"), old_rom_data, rom: Rc::new(rom) })
     }
