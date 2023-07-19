65e193884440575eba8d2095fa8c3b8ffbb47d83
Adanos020 <adanos020@gmail.com>
2023-07-19 22:42:11 +0100

    Refactor keyboard shortcuts.

--- src/ui/editor_prototypes/sprite_map_editor/keyboard_shortcuts/mod.rs
+++ src/ui/editor_prototypes/sprite_map_editor/keyboard_shortcuts/mod.rs
@@ -1,28 +1,13 @@
+mod data;
+
+pub(super) use data::*;
 use duplicate::duplicate;
-use egui::{vec2, Event, Key, KeyboardShortcut, Modifiers, Pos2, Ui};
+use egui::{vec2, Event, Key, Pos2, Ui};
 use smwe_math::coordinates::{OnCanvas, OnScreen};
 use smwe_render::tile_renderer::TileJson;
 
 use super::UiSpriteMapEditor;
 use crate::ui::editing_mode::{EditingMode, SnapToGrid};
-
-pub(super) const SHORTCUT_UNDO: KeyboardShortcut = KeyboardShortcut::new(Modifiers::COMMAND, Key::Z);
-pub(super) const SHORTCUT_REDO: KeyboardShortcut = KeyboardShortcut::new(Modifiers::COMMAND, Key::Y);
-
-pub(super) const SHORTCUT_SELECT_ALL: KeyboardShortcut = KeyboardShortcut::new(Modifiers::COMMAND, Key::A);
-pub(super) const SHORTCUT_UNSELECT_ALL: KeyboardShortcut = KeyboardShortcut::new(Modifiers::NONE, Key::Escape);
-
-pub(super) const SHORTCUT_DELETE_SELECTED: KeyboardShortcut = KeyboardShortcut::new(Modifiers::NONE, Key::Delete);
-
-pub(super) const SHORTCUT_ZOOM_IN: KeyboardShortcut = KeyboardShortcut::new(Modifiers::COMMAND, Key::PlusEquals);
-pub(super) const SHORTCUT_ZOOM_OUT: KeyboardShortcut = KeyboardShortcut::new(Modifiers::COMMAND, Key::Minus);
-
-pub(super) const SHORTCUT_MODE_INSERT: KeyboardShortcut = KeyboardShortcut::new(Modifiers::NONE, Key::Num1);
-pub(super) const SHORTCUT_MODE_SELECT: KeyboardShortcut = KeyboardShortcut::new(Modifiers::NONE, Key::Num2);
-pub(super) const SHORTCUT_MODE_ERASE: KeyboardShortcut = KeyboardShortcut::new(Modifiers::NONE, Key::Num3);
-pub(super) const SHORTCUT_MODE_PROBE: KeyboardShortcut = KeyboardShortcut::new(Modifiers::NONE, Key::Num4);
-pub(super) const SHORTCUT_MODE_FLIP_HORIZONTALLY: KeyboardShortcut = KeyboardShortcut::new(Modifiers::NONE, Key::Num5);
-pub(super) const SHORTCUT_MODE_FLIP_VERTICALLY: KeyboardShortcut = KeyboardShortcut::new(Modifiers::NONE, Key::Num6);
 
 impl UiSpriteMapEditor {
     pub(super) fn handle_input(&mut self, ui: &Ui) {
@@ -69,30 +54,34 @@ impl UiSpriteMapEditor {
     }
 
     fn kb_shortcut_move_selection(&mut self, ui: &Ui) {
-        let move_distance = if ui.input(|input| input.modifiers.shift_only()) { self.tile_size_px } else { 1. };
-        duplicate! {
-            [
-                key          offset;
-                [ArrowUp]    [vec2(0., -move_distance)];
-                [ArrowDown]  [vec2(0.,  move_distance)];
-                [ArrowLeft]  [vec2(-move_distance, 0.)];
-                [ArrowRight] [vec2( move_distance, 0.)];
-            ]
-            if ui.input(|input| input.key_pressed(Key::key)) {
-                self.move_selected_tiles_by(
-                    OnCanvas(offset),
-                    ui.input(|input| input.modifiers.shift_only().then_some(SnapToGrid::default())),
-                );
+        ui.input(|input| {
+            let move_distance = if input.modifiers.shift_only() { self.tile_size_px } else { 1. };
+            duplicate! {
+                [
+                    key          offset;
+                    [ArrowUp]    [vec2(0., -move_distance)];
+                    [ArrowDown]  [vec2(0.,  move_distance)];
+                    [ArrowLeft]  [vec2(-move_distance, 0.)];
+                    [ArrowRight] [vec2( move_distance, 0.)];
+                ]
+                if input.key_pressed(Key::key) {
+                    self.move_selected_tiles_by(
+                        OnCanvas(offset),
+                        input.modifiers.shift_only().then_some(SnapToGrid::default()),
+                    );
+                }
             }
-        }
+        });
     }
 
     fn handle_zoom(&mut self, ui: &Ui) {
-        if ui.input_mut(|input| input.zoom_delta() > 1. || input.consume_shortcut(&SHORTCUT_ZOOM_IN)) {
-            self.zoom = 4.0f32.min(self.zoom + 0.25);
-        } else if ui.input_mut(|input| input.zoom_delta() < 1. || input.consume_shortcut(&SHORTCUT_ZOOM_OUT)) {
-            self.zoom = 1.0f32.max(self.zoom - 0.25);
-        }
+        ui.input_mut(|input| {
+            if input.zoom_delta() > 1. || input.consume_shortcut(&SHORTCUT_ZOOM_IN) {
+                self.zoom = 4.0f32.min(self.zoom + 0.25);
+            } else if input.zoom_delta() < 1. || input.consume_shortcut(&SHORTCUT_ZOOM_OUT) {
+                self.zoom = 1.0f32.max(self.zoom - 0.25);
+            }
+        });
     }
 
     fn kb_shortcut_copy(&mut self, ui: &Ui) {
